e0dc778097cd5d06b6199147a6e2308e0f577a8b
Bernat Jufré <info@behind.design>
2017-09-01 18:49:32 +0200

    Adding travis

--- mix.exs
+++ mix.exs
@@ -1,11 +1,11 @@
 defmodule RavenxPusher.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [app: :ravenx_pusher,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
